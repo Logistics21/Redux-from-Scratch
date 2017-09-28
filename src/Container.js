4f15d0a3095bbbe3dee8a8f91f9d4f6a69395e57
David Halsey <dhalsey212@gmail.com>
2017-09-28 12:16:54 -0400

    Create subscription and notice system

--- src/Container.js
+++ src/Container.js
@@ -1,3 +1,17 @@
+import React, { PropTypes } from 'react';
+
+const storeShape = PropTypes.shape({
+  subscribe: PropTypes.func.isRequired,
+  dispatch: PropTypes.func.isRequired,
+  getState: PropTypes.func.isRequired
+})
+
+const subscriptionShape = PropTypes.shape({
+  trySubscribe: PropTypes.func.isRequired,
+  notifyNestedSubs: PropTypes.func.isRequired
+})
+
+
 HOC = connect(mapStateToPops, mapDispatchToProps)
 
 Container = HOC(wrappedComponent)
